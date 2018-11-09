af50c1b7cd773b7e27c85a53a4aebecb83dc9809
Drewsup123c <drewsup123c@gmail.com>
2018-11-09 10:37:08 -0700

    added post functionality to projects

--- api/server.js
+++ api/server.js
@@ -25,6 +25,36 @@ server.get('/api/projects',(req,res)=>{
         })
 });
 
+server.get("/api/projects/:id",(req,res)=>{
+    const { id } = req.params;
+
+    projectDb.get(id)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(error => {
+            res.status(500).json("Unable to get the projects ", error)
+        })
+});
+
+server.post('/api/projects', async (req, res) => {
+    console.log('body : ', req.body);
+    try {
+        const projectData = req.body;
+        const projectId = await projectDb.insert(projectData);
+        const project = await projectDb.get(projectId.id);
+        res.status(201).json(project);
+    } catch (error) {
+        let message = 'error creating the project';
+
+        if (error.errno === 19) {
+            message = 'please provide the name of the project';
+        }
+
+        res.status(500).json({ error: "There was an error while saving the project to the database" });
+    }
+});
+
 //   Actions Code
 
 
