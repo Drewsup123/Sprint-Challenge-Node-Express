96fd1b2e2bbd5edd4075b2146e2e9ee15764372f
Drewsup123c <drewsup123c@gmail.com>
2018-11-09 10:31:11 -0700

    added knex as a dependency, created get request for projects

--- api/server.js
+++ api/server.js
@@ -3,9 +3,30 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const server = express();
 
+const projectDb = require('../data/helpers/projectModel');
+const actionDb = require('../data/helpers/actionModel');
+
 server.use(express.json());
 server.use(helmet()); 
 server.use(morgan('dev'));
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+});
+
+//   Projects Code
+server.get('/api/projects',(req,res)=>{
+    projectDb.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(error => {
+            res.status(500).json("can not find the projects ",error)
+        })
+});
+
+//   Actions Code
+
 
 
 
